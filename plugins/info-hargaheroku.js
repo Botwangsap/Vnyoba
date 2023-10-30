b029068c39bca6f03e8792cfb66738e9ec9756cd
Nyanpasuu <107746504+Botwangsap@users.noreply.github.com>
2023-10-30 21:47:03 +0700

    Add files via upload

---
+++ plugins/info-hargaheroku.js
@@ -0,0 +1,80 @@
+let handler = async (m, { conn }) => {
+let sewa = `
+_LIST HARGA AKUN HEROKU_
+__________________________________________
+*🔰Ready Akun Heroku/Bulan = 20k🔰*
+
+Mendapatkan 8 CPU 8375C-2,90 GHz
+Ram 61,50GB+
+Fast Respon
+Web Di Close Tetap Aktif
+Hemat Penyimpanan
+Aktif 24 Jam Nonstop
+Bisa Request Username
+Garansi 1 Minggu
+
+🔰Cocok Untuk Menjalankan
+-Python
+-Nodejs
+-Bot Telegram
+-Bot WhatsApp
+
+🇮🇩🇮🇩🇮🇩🇮🇩🇮🇩🇮🇩🇮🇩🇮🇩🇮🇩🇮🇩🇮🇩🇮🇩🇮🇩🇮🇩🇮🇩🇮🇩🇮🇩
+『ʜᴇʀᴏᴋᴜ ᴀᴄᴄᴏᴜɴᴛ』
+
+*ʀᴇᴀᴅʏ ᴀᴄᴄᴏᴜɴᴛ ʜᴇʀᴏᴋᴜ*
+ᴋᴇ ᴜɴᴛᴜɴɢᴀɴ:
+•ʀᴀᴍ 61,50GB
+•ᴄᴘᴜ 8 CPU 8375C-2,90 GHz
+•ᴘᴇʀ ᴀᴋᴜɴ 1000 ᴅʏɴᴏs
+•ʙɪsᴀ ʙᴜᴀᴛ 5 sᴇʀᴠᴇʀ
+•ғᴀsᴛ ʀᴇsᴘᴏɴ 2 ᴘɪʟɪʜᴀɴ ʀᴇɢɪᴏɴ
+•ʙᴇʀɢᴀʀᴀɴsɪ 1 ᴍɪɴɢɢᴜ
+•ʜᴇᴍᴀᴛ ᴘᴇɴʏɪᴍᴘᴀɴᴀɴ & ᴋᴜᴏᴛᴀ
+•ᴡᴇʙ ᴅɪ ᴄʟᴏsᴇ ᴍᴀsɪʜ ɴʏᴀʟᴀ
+•ʙɪsᴀ ʀᴇǫ ᴜsᴇʀɴᴀᴍᴇ
+•ᴍᴀsᴀ ᴀᴋᴛɪғ 1ʙᴜʟᴀɴ ᴋᴀʟᴏ ʜᴏᴋɪ ʙɪsᴀ ʟᴇʙɪʜ
+•ғʀᴇᴇ ᴘᴇɴɢᴀᴊᴀʀᴀɴ ᴊɪᴋᴀ ɢᴀ ᴘᴀʜᴀᴍ
+•ʙɪsᴀ ᴄᴏɴɴᴇᴄᴛ ᴋᴇ ɢɪᴛʜᴜʙ (sᴄ ᴊᴀᴅɪ ᴀᴍᴀɴ)
+•ᴅʟʟ (ʙɪsᴀ ᴛᴀɴʏᴀ ᴋᴇ ᴀᴅᴍɪɴ)
+
+ʙɪsᴀ ᴅɪɢᴜɴᴀᴋᴀɴ ᴜɴᴛᴜᴋ:
+•ᴘʏᴛʜᴏɴ
+•ɴᴏᴅᴇᴊs
+•ʀᴜɴ ᴡᴇʙsɪᴛᴇ/ʀᴇsᴛ ᴀᴘɪ
+•ʀᴜɴ ʙᴏᴛ ᴛᴇʟᴇɢʀᴀᴍ
+•ʀᴜɴ ʙᴏᴛ ᴡʜᴀᴛsᴀᴘᴘ
+•ʀᴜɴ ʙᴏᴛ ᴅɪsᴄᴏʀᴅ
+
+*ʜᴀʀɢᴀ?:* 20ᴋ/ᴀᴋᴜɴ
+*ᴍɪɴᴀᴛ? ʜᴜʙ:* https://wa.me/6281998425782
+
+『ᴘᴇʀʙᴇᴅᴀᴀɴ ᴀᴍᴀ ᴘᴀɴᴇʟ』
+
+🗣️: ᴀᴘᴀ ʙᴇᴅᴀɴʏᴀ ᴀᴍᴀ ᴘᴀɴᴇʟ?
+
+
+👤: ᴋᴀʟᴏ ᴋɪᴛᴀ ʙᴇʟɪ ᴘᴀɴᴇʟ ᴋᴀɴ ʜᴀɴʏᴀ ᴅᴀᴘᴀᴛ 1 sᴇʀᴠᴇʀ
+       ᴊɪᴋᴀ ᴋɪᴛᴀ ᴍᴇɴɢɢᴜɴᴀᴋᴀɴ ʜᴇʀᴏᴋᴜ ᴋɪᴛᴀ ʙɪsᴀ 
+       ᴍᴇᴍʙᴜᴀᴛ 5 sᴇʀᴠᴇʀ sᴇᴋᴀʟɪɢᴜs ʟᴏʜ
+       
+       
+🗣️: ʙᴇʀᴀᴘᴀ ʙᴀɴɢ 1 ᴀᴋᴜɴ?
+
+
+👤: ʜᴀɴʏᴀ 20 ʀɪʙᴜ ᴀɴᴅᴀ sᴜᴅᴀʜ ᴍᴇɴᴅᴀᴘᴀᴛᴋᴀɴ ɴʏᴀ
+       ʙᴀʏᴀɴɢɪɴ ɴɪʜ ᴊɪᴋᴀ ᴋᴀᴍᴜ ʙᴇʟɪ ᴘᴀɴᴇʟ ᴅɪ ᴏʀɢ² ᴋᴀɴ ʜᴀɴʏᴀ
+       ᴅᴀᴘᴀᴛ ʙᴇʙᴇʀᴀᴘᴀ ᴄᴘᴜ ᴅᴀɴ ʀᴀᴍ, ᴋᴀʟᴏ ᴅɪ ʜᴇʀᴏᴋᴜ ᴋᴀᴍᴜ
+       sᴜᴅᴀʜ ᴅᴀᴘᴀᴛ *8 CPU 8375C-2,90 GHz* ᴅᴀɴ ʀᴀᴍ *61,50GB+* 
+
+Ingin tes speed bot/melihat cpu/ram?
+
+Nomor bot :  https://wa.me/6283191486089?text=.info
+`
+conn.reply(m.chat, sewa, m)
+}
+handler.help = ['hargaheroku']
+handler.tags = ['main']
+handler.command = /^(hargaheroku)$/i
+
+export default handler
